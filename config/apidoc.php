639f612cf027ce97a0a1271bfea34cabb898d7ae
Christian Busch <kontakt@christian-busch.com>
2020-06-24 22:55:42 +0200

    feat: added filtered address transfers

--- config/apidoc.php
+++ config/apidoc.php
@@ -53,7 +53,7 @@ return [
      * The base URL to be used in examples and the Postman collection.
      * By default, this will be the value of config('app.url').
      */
-    'base_url' => 'https://openapi.nkn.org/api/v1',
+    'base_url' => 'https://openapi.nkn.org',
 
     /*
      * Generate a Postman collection in addition to HTML docs.
