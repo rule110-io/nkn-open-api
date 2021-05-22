1b4ca6984faba3a624eaaee7ba908c3d0a398cc6
Christian Busch <kontakt@christian-busch.com>
2021-05-22 12:03:03 +0200

    logs

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -8,6 +8,7 @@ use Closure;
 class IpMiddleware{
     public function handle($request, Closure $next)
     {
+        Log::info('['.$request->ip().']:'.config('allowed_ips'));
         if (!in_array($request->ip(),config('allowed_ips')) {
             abort(403, 'Access denied');
         }
