ec016399d09c27b2813c5467e6ee537048184da3
Christian Busch <kontakt@christian-busch.com>
2021-05-22 11:28:51 +0200

    reactivated middleware

--- app/Http/Middleware/LogRouteMiddleware.php
+++ app/Http/Middleware/LogRouteMiddleware.php
@@ -3,14 +3,15 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Log;
+use Closure;
 
+class LogRouteMiddleWare{
+    public function handle($request, Closure $next)
+    {
+        Log::channel('accessRoutes')->info('['.$request->ip().']:'.$request->fullUrl());
 
-public function handle($request, Closure $next)
-{
-    Log::channel('accessRoutes')->info('['.$request->ip().']:'.$request->fullUrl());
-
-    return $next($request);
-};
-
+        return $next($request);
+    }
+}
 
 
