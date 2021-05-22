abd349c3f33faa5fe16f6813cad5e5b804fc7b8e
Christian Busch <kontakt@christian-busch.com>
2021-05-22 11:21:42 +0200

    another fix

--- app/Http/Middleware/LogRouteMiddleware.php
+++ app/Http/Middleware/LogRouteMiddleware.php
@@ -5,13 +5,12 @@ use Illuminate\Http\Request;
 use Log;
 
 
-class LogRouteMiddleWare {
-    public function handle($request, Closure $next)
-    {
-        Log::channel('accessRoutes')->info('['.$request->ip().']:'.$request->fullUrl());
+public function handle($request, Closure $next)
+{
+    Log::channel('accessRoutes')->info('['.$request->ip().']:'.$request->fullUrl());
 
-        return $next($request);
-    }
+    return $next($request);
 }
+
 
 
