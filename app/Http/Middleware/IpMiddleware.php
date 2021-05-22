a8cbbfc93a15bffdf8d2efda0b4012eedada5bb2
Christian Busch <kontakt@christian-busch.com>
2021-05-22 12:05:21 +0200

    fix

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -8,10 +8,10 @@ use Closure;
 class IpMiddleware{
     public function handle($request, Closure $next)
     {
-        Log::info('['.$request->ip().']:'.config('allowed_ips'));
+
         if (!in_array($request->ip(),config('allowed_ips')) {
             abort(403, 'Access denied');
-        }
+        };
 
         return $next($request);
     }
