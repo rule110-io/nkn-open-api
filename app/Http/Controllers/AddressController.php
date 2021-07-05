4304f9e3c7c3ba30eaad7d4ca9e00c279ffe3687
Christian Busch <kontakt@christian-busch.com>
2021-07-05 23:08:42 +0200

    debug

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -43,10 +43,11 @@ class AddressController extends Controller
         $count = Cache::remember('sumAddresses', config('nkn.update-interval'), function (){
             return DB::table('address_statistics')->count();
         });
+        dd($count[0]);
         // Create a response and modify a header value
         $response = response()->json([
             'addresses' => $addresses,
-            'sumAddresses' => $count[0]->count
+            'sumAddresses' => $count
         ]);
 
         return $response;
